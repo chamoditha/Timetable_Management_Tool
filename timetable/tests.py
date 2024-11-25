64ed20b8406743fd69bed176233172b4049a9ae7
HashiniGamage <134942403+HashiniGamage@users.noreply.github.com>
2024-11-25 22:28:54 +0530

    Update tests.py

--- timetable/tests.py
+++ timetable/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class RegistrationTestCase(TestCase):
+
+    def setUp(self):
+        # Create an initial user
+        User.objects.create_user(username="johndoe", password="Test@123")
+
+    def test_duplicate_username(self):
+        # Attempt to register a user with the same username
+        response = self.client.post('/register/', {
+            'username': 'johndoe',  # Duplicate username
+            'password': 'NewPassword@123',
+            'confirm_password': 'NewPassword@123'
+        })
+
+        # Assert that the response contains the expected error message
+        self.assertContains(response, "Username already exists", status_code=200)
+
+        # Assert that no additional user was created
+        self.assertEqual(User.objects.filter(username="johndoe").count(), 1)
